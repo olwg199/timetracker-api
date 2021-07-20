6a9adf6a75d58d6feade8142ac80df5b982b7cf7
Oleh Yastrebov <olwg199@gmail.com>
2021-07-20 12:05:27 -0400

    Add morgan dependency for logging

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const port = process.env.PORT || 3000;
 const express = require("express");
+const morgan = require("morgan");
 
 const app = express();
 
 const taskRoutes = require("./api/routes/tasks");
 
+app.use(morgan('dev'));
 app.use('/tasks', taskRoutes);
 
 app.listen(port, function () {
