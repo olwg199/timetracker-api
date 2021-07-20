5cffff35fc34648a80c042157c1c541b688731a8
Oleh Yastrebov <olwg199@gmail.com>
2021-07-20 12:17:11 -0400

    Add error handling

--- app.js
+++ app.js
@@ -6,8 +6,24 @@ const app = express();
 
 const taskRoutes = require("./api/routes/tasks");
 
+// Logging
 app.use(morgan('dev'));
+
 app.use('/tasks', taskRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error("Not found");
+    error.status = 404;
+    next(error);
+});
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+});
 
 app.listen(port, function () {
     console.log("Server is running on port " + port);
