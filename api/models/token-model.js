ec2c013e478d53fca9d362a08f0860712415d373
Oleh Yastrebov <olwg199@gmail.com>
2021-08-07 12:34:29 -0400

    Add token schema

---
+++ api/models/token-model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const tokenSchema = mongoose.Schema({
+    refreshToken: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+});
+
+module.exports = mongoose.model("Token", userSchema);
