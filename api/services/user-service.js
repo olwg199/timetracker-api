f7e4a2cee04ca7ab7caee3225c57f7726bb1d0a9
Oleh Yastrebov <olwg199@gmail.com>
2021-08-07 13:38:12 -0400

    Add signup functionality for user service

--- api/services/user-service.js
+++ api/services/user-service.js
@@ -1,6 +1,8 @@
 const bcrypt = require("bcrypt");
 const uuid = require("uuid");
 const mailService = require("./mail-service");
+const tokenService = require("./token-service");
+const UserDto = require("../dtos/user-dto");
 
 const User = require("../models/user");
 
@@ -17,6 +19,15 @@ class UserService {
 
         const user = await User.create({ username, password: hashPassword, email, activationLink });
         await mailService.sendActivationMail(email, activationLink);
+
+        const userDto = new UserDto(user);
+        const tokens = tokenService.generateToken({ ...userDto });
+        await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+        return {
+            ...tokens,
+            user: userDto
+        };
     }
 }
 
